05e6702962e56d8423953a88920ca0421ff9297b
Yoga Pratama <yogapr.dev@gmail.com>
2023-11-10 10:44:05 +0700

    fix: change experiences

--- src/assets/index.js
+++ src/assets/index.js
@@ -25,6 +25,8 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import osi from './company/osi.png'
+import labkom from './company/labkom.png'
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -56,6 +58,8 @@ export {
   shopify,
   starbucks,
   tesla,
+  osi,
+  labkom,
   carrent,
   jobit,
   tripguide,
